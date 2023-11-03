25e242a9cd164fed7ccd0828cf82c3fbf085de8f
Lenka Hudčeková <lenkahudcekova@gmail.com>
2023-11-03 08:03:30 +0100

    vyřešení celého úkolu

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const bodyElement = document.querySelector("body")
 bodyElement.style.backgroundColor = "#e9e9e9";
+const news =document.querySelector (".news")
+news.style.backgroundColor = "white"
+news.style.width = "60rem";
+
+const newsTitle = document.querySelector("h1")
+newsTitle.classList.add("news__title")
+newsTitle.textContent = "Aktuální novinky"
+
+const prvniZprava = document.querySelector("#zprava1")
+prvniZprava.classList.add("post--main")
+
+const obrazek = document.querySelector("#zprava3")
+const obrazek3 = obrazek.querySelector(".post__picture")
+obrazek3.src = "images/zprava3-novy.jpg"
+
+
 
 
